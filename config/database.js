c3f94fd38ffc5cefe8ac4b2299aa4e400d20e8fc
Gabriel Ramirez <68472070+GabrielRamirezDev@users.noreply.github.com>
2020-09-25 23:56:56 -0400

    Coffee Shop Order System

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://dbUser:he11oworld@cluster0.itpju.mongodb.net/cluster0', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'coffee'
+};
